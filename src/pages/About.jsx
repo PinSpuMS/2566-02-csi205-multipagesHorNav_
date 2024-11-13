437a1d5c0f8963c84d0002b5f79a4e5193a1544a
PinSpuMS <pin.ch@spulive.net>
2024-11-13 09:29:14 +0700

    Delete source code

--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const About = () => {
-  return (
-    <div style={{ height: '500px', border: '1px solid' }}>
-      <h1>ABOUT</h1>
-    </div>
-  )
-}
--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Home = () => {
-  return (
-    <div style={{ height: '500px', border: '1px solid' }}>
-      <h1>HOME</h1>
-    </div>
-  )
-}
